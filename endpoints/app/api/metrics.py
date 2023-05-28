43a4ac7f112c0d70155884adf53e084d6368a92f
Hynek Kydlicek <kydlicek.hynek@gmail.com>
2023-05-28 19:04:20 +0200

    fixed apis

--- endpoints/app/api/metrics.py
+++ endpoints/app/api/metrics.py
@@ -5,19 +5,22 @@ from app.utils import create_events_evolution_graph
 from flask import jsonify, send_file
 from app.models import Event
 from app.api import bp
-from app.api.errors import bad_request, error_response, info_response
+from app.api.errors import info_response
+from app import db
 
+
+EVENT_TYPES = ['PullRequestEvent', 'IssuesEvent', 'WatchEvent']
 
 
 @bp.route("/metrics/average_pr_time/<repo_name>", methods=['GET'])
 def average_pr_time(repo_name: str):
     pr_events: List[Event] = (
-        Event.query
+        db.session.query(Event.created_at)
         .filter_by(type="PullRequestEvent", repo_name=repo_name)
         .order_by(Event.created_at).all()
     )
     if len(pr_events) < 2:
-        return info_response('Not enough data to compute average PR time'), 204
+        return info_response(200, 'Not enough data to compute average PR time')
 
     times = [event.created_at for event in pr_events]
     average_delta = sum(
@@ -34,12 +37,19 @@ def average_pr_time(repo_name: str):
 def event_counts(offset: int):
     offset_datetime = datetime.utcnow() - timedelta(minutes=offset)
     event_counts = (
-        db_session.query(func.count(Event.type))
+        db.session.query(func.count(Event.type))
         .filter(Event.created_at >= offset_datetime)
         .group_by(Event.type)
         .all()
     )
-    return dict(event_counts)
+    # Add 0 for missing event types
+    event_counts = dict(event_counts)
+    for event in EVENT_TYPES:
+        if event not in event_counts:
+            event_counts[event] = 0
+
+
+    return jsonify(event_counts)
 
 
 
@@ -47,11 +57,8 @@ def event_counts(offset: int):
 def event_per_minute_image(offset: int):
     now = datetime.utcnow()
     offset_datetime = now - timedelta(minutes=offset)
-    events = db_session.query(Event.type, Event.created_at).filter(Event.created_at >= offset_datetime).all()
-    if len(events) == 0:
-        return '', 204
-
-    buf = create_events_evolution_graph(events, offset_datetime, now)
+    events = db.session.query(Event.type, Event.created_at).filter(Event.created_at >= offset_datetime).all()
+    buf = create_events_evolution_graph(events, offset_datetime, now, EVENT_TYPES)
 
     # Save it to a BytesIO object
     return send_file(buf, mimetype='image/png')
